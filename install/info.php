3f0e6acddad82560e4ba02963df12072efc15c96
webmanufaktur <info@webmanufaktur.net>
2025-01-19 22:31:49 +0100

    2025-01-19

---
+++ install/info.php
@@ -0,0 +1,7 @@
+<?php namespace ProcessWire;
+if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = [
+	'title' => "ProcessWire Base Starter 2025", 
+	'summary' => "", 
+	'screenshot' => "pw-base-cover.png"
+];
